3efeb14434dbfc6c8dbda455d420330254860535
Eugene <evgentjkuznecov@MacBook-Air-Evgenij.local>
2019-07-14 19:15:08 +0700

    remake search trip methid

--- src/routes/Trip.js
+++ src/routes/Trip.js
@@ -1,66 +1,98 @@
-import express from 'express'
-import authentication from './../middleware/authentication'
-import Trip from './../models/Trip'
-import _ from 'lodash'
-const mongoose = require('mongoose')
+import express from "express";
+import authentication from "./../middleware/authentication";
+import Trip from "./../models/Trip";
+import _ from "lodash";
+const mongoose = require("mongoose");
 
-const router = express.Router()
+const router = express.Router();
 
 const updateBooking = (response, tripId, updatedField, action) => {
-  Trip.update({ _id: tripId }, updatedField )
-    .then((result)=> {
-        response.status(201).json({
-          message: 'Driver ' + action +'s your trip'
-        })
+  Trip.update(
+    {
+      _id: tripId
+    },
+    updatedField
+  )
+    .then(result => {
+      response.status(201).json({
+        message: "Driver " + action + "s your trip"
+      });
     })
-    .catch((err)=>{
+    .catch(err => {
       response.status(500).json({
         message: err
-      })
-    })
-}
+      });
+    });
+};
 
-router.get('/search', (req, res, next)=> {
-  const {from, to} = req.query
-  let filter = {}
-  if(from && to) {
-     filter = {
-       'from.lat': from.split(',')[0],
-       'from.lng': from.split(',')[1],
-       'to.lat': to.split(',')[0],
-       'to.lng': to.split(',')[1]
-     }
+router.get("/search", (req, res, next) => {
+  const { from, to, email } = req.query;
+  let filter = {};
+  if (from && to) {
+    filter = {
+      "from.lat": from.split(",")[0],
+      "from.lng": from.split(",")[1],
+      "to.lat": to.split(",")[0],
+      "to.lng": to.split(",")[1]
+    };
   }
-
+  let populateFilter = {
+    path: 'owner',
+    select: 'image username phone age email',
+  }
+  if(email) {
+    populateFilter['match'] = {
+      email
+    }
+  }
   Trip.find(filter)
-  .populate('owner', 'image username phone age email')
-  .exec()
-  .then((results)=> {
-    res.status(200).json({
-      results
+    .populate(populateFilter)
+    .exec()
+    .then(results => {
+      res.status(200).json({
+        results: results.filter((item)=> {
+          return !!item.owner
+        })
+      });
     })
-  }).catch((error)=>{
-    res.status(500).json({
-      results: {
-        message: error
-      }
+    .catch(error => {
+      res.status(500).json({
+        results: {
+          message: error
+        }
+      });
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  Trip.findOne(id)
+    .populate("owner", "image username phone age email")
+    .then(results => {
+      res.status(200).json({
+        results
+      });
     })
-  })
-})
+    .catch(error => {
+      res.status(500).json({
+        results: {
+          message: error
+        }
+      });
+    });
+});
 
-router.get('/:id', (req, res, next)=> {
-  const {id} = req.params
-  Trip.findOne(id).populate('owner bookedPeople')
-  .then((results)=>{
-    res.status(200).json({
-      results
-    })
-  })
-})
-
-
-router.post('/create', authentication, (req, res, next)=>{
-  const {from, to, dateStart, dateFinished, maxPeople, occupiedPlaces, cost, carModel } = req.body
+router.post("/create", authentication, (req, res, next) => {
+  const {
+    from,
+    to,
+    dateStart,
+    dateFinished,
+    maxPeople,
+    occupiedPlaces,
+    cost,
+    carModel
+  } = req.body;
   const trip = new Trip({
     _id: new mongoose.Types.ObjectId(),
     owner: req.userId,
@@ -77,62 +109,69 @@ router.post('/create', authentication, (req, res, next)=>{
     cancelPeople: []
   });
 
-
   trip
     .save()
-    .then((results)=>{
-    res.status(201).json({
-      results
+    .then(results => {
+      res.status(201).json({
+        results
+      });
     })
-  }).catch((error)=>{
-    res.status(500).json({
-      results: {
-        message: error
-      }
-    })
-  })
+    .catch(error => {
+      res.status(500).json({
+        results: {
+          message: error
+        }
+      });
+    });
+});
 
-})
-
-
-router.put('/update/:id', authentication, (req, res, next)=> {
-  const {id} = req.params
+router.put("/update/:id", authentication, (req, res, next) => {
+  const { id } = req.params;
   const filterParams = [];
-  const body = req.body
+  const body = req.body;
   let updatedParams = {};
-  _.forEach(body, (value, prop)=> {
-      if(filterParams.indexOf(prop) !== -1) {
-        updatedParams[prop] = value;
-      }
-  })
+  _.forEach(body, (value, prop) => {
+    if (filterParams.indexOf(prop) !== -1) {
+      updatedParams[prop] = value;
+    }
+  });
   Trip.findOneAndUpdate(
-    { _id: id },
-    { ...updatedParams, confirmed: true },
-    { new: true }
-  ).then((newRecord)=>{
-    res.status(201).json({
-      message: 'Trip updated',
-      results: newRecord,
-      type: 'POST'
+    {
+      _id: id
+    },
+    {
+      ...updatedParams,
+      confirmed: true
+    },
+    {
+      new: true
+    }
+  )
+    .then(newRecord => {
+      res.status(201).json({
+        message: "Trip updated",
+        results: newRecord,
+        type: "POST"
+      });
     })
-  }).catch((error)=>{
-    req.status(500).json({
-      results: {
-        message: error
-      }
-    })
-  })
+    .catch(error => {
+      req.status(500).json({
+        results: {
+          message: error
+        }
+      });
+    });
+});
 
-})
-
-router.delete('/delete/:id', (req, res, next)=>{
-  const {id} = req.params.id
-  Trip.remove({_id: id}).then((result)=>{
+router.delete("/delete/:id", (req, res, next) => {
+  const { id } = req.params.id;
+  Trip.remove({
+    _id: id
+  }).then(result => {
     res.status(200).json({
-      results: 'Trip removed'
-    })
-  })
-})
+      results: "Trip removed"
+    });
+  });
+});
 
-
-module.exports = router
+module.exports = router;
